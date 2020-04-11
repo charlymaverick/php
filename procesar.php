f4cee3f19584167974f57fa761d8d988ec366510
Charly Maverick <45545366+charlymaverick@users.noreply.github.com>
2020-04-11 16:39:49 -0500

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,16 @@
+
+<?php
+
+$nombre = $_GET['nombre'];
+$apellido =  $_GET['apellido'];  
+$correo = $_GET['correo'];  
+
+echo "el nombre recibido en php es  " .$nombre. "</br>";
+echo "el apellido recibido en php es  " .$apellido. "</br>";
+echo "el EMAIL recibido en php es  " .$correo. "</br>";
+
+
+
+?>
+
+
